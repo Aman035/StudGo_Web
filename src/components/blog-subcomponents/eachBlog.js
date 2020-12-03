27c117799abbad6c0d910f67274820c1fbbf3772
Aman Gupta <guptaaman200115@gmail.com>
2020-12-03 18:24:12 +0530

    UI issue fix

--- src/components/blog-subcomponents/eachBlog.js
+++ src/components/blog-subcomponents/eachBlog.js
@@ -11,7 +11,8 @@ import { Link } from 'react-router-dom';
 const useStyles = makeStyles(() => ({
   root: {
     margin : 'auto',
-    maxWidth: 700,
+    width : 700,
+    maxWidth: '90%',
     borderRadius: 20,
   },
   content: {
